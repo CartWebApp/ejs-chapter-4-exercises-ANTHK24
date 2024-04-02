89dc01923a5a16092e6d1e6a8409b7add642729c
kaesheree <143832533+kaesheree@users.noreply.github.com>
2024-04-01 18:02:46 -0700

    sum range done and tested with node.js

--- sum-range.js
+++ sum-range.js
@@ -1,9 +1,20 @@
 function range(start, end, step = 1) {
-  // Add code.
+  let array =[];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+return array;
 }
 
 function sum(numbers) {
-  // Add code.
+  let total = 0;
+  for (let value of numbers) {
+    total += value;
+  }
+  return total;
 }
 
 
