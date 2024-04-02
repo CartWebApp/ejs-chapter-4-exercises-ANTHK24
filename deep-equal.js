c45bc068e5de519853cfb2545944ebf94dae97be
kaesheree <143832533+kaesheree@users.noreply.github.com>
2024-04-01 17:50:56 -0700

    finished deep equal with comments

--- deep-equal.js
+++ deep-equal.js
@@ -1,5 +1,26 @@
-function deepEqual(value, reference) {
-  // Add code.
+function deepEqual(a, b) {
+  // if a and b are exactly the same, it will return true
+  if (a === b) return true;
+
+  // this code checks to see if a is empty/not an object and if none of the conditions of the code get met, it will display false in the console.
+  if (a == null || typeof a != "object" ||
+    b == null || typeof b != "object") return false;
+
+
+  // makes two arrays and stores the names of them, a and b respectively
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  //checks if the objects in the code provided above are not equal and makes sure both have the same amount of properties available
+  if (keysA.length != keysB.length) return false;
+
+  // uses a loop to check if the keys have equality.
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+
+
+  }
+
+  return true;
 }
 
 
@@ -8,10 +29,10 @@ function deepEqual(value, reference) {
 
 
 // tests
-let obj = {here: {is: "an"}, object: 2};
+let obj = { here: { is: "an" }, object: 2 };
 console.log(deepEqual(obj, obj));
 // → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // → true
